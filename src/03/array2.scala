60c258df7f1328de44fef24c981e9f3928cda30d
dsanno <daiki.sanno@gmail.com>
2013-01-27 12:01:01 +0900

    add.add programs associate with array, list, set, map, function.

---
+++ src/03/array2.scala
@@ -0,0 +1,6 @@
+val greetStrings2 = new Array[String](3)
+greetStrings2.update(0, "Hello")
+greetStrings2.update(1, ", ")
+greetStrings2.update(2, "world!\n")
+for (i <- 0.to(2))
+  print(greetStrings.apply(i))
---
+++ src/03/tuple.scala
@@ -0,0 +1,11 @@
+val pair = (99, "Luftbaloons")
+println(pair._1)
+println(pair._2)
+
+val tuple = ('u', 'r', "the", 1, 4, "me")
+println(tuple._1)
+println(tuple._2)
+println(tuple._3)
+println(tuple._4)
+println(tuple._5)
+println(tuple._6)
